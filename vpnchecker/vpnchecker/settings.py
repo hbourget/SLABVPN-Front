64ed1315d853a4aa7d36b834529fe214589b2921
hugobourget <hugobourget@protonmailcom>
2024-12-09 11:42:29 +0100

    feat: pgadmin

--- vpnchecker/vpnchecker/settings.py
+++ vpnchecker/vpnchecker/settings.py
@@ -71,11 +71,11 @@ WSGI_APPLICATION = 'vpnchecker.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get("DATABASE_NAME"),
-        'USER': os.environ.get("DATABASE_USER"),
-        'PASSWORD': os.environ.get("DATABASE_PASSWORD"),
-        'HOST': os.environ.get("DATABASE_HOST"),
-        'PORT': os.environ.get("DATABASE_PORT"),
+        'NAME': os.environ.get("POSTGRES_DB"),
+        'USER': os.environ.get("POSTGRES_USER"),
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
+        'HOST': os.environ.get("POSTGRES_HOST"),
+        'PORT': os.environ.get("POSTGRES_PORT"),
     }
 }
 
