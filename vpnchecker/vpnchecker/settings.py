2ada4ed2d68bd1e8752b86dcb5009e2e9fa69196
hugobourget <hugobourget@protonmailcom>
2024-12-17 10:02:30 +0100

    fix: cleanup

--- vpnchecker/vpnchecker/settings.py
+++ vpnchecker/vpnchecker/settings.py
@@ -15,21 +15,9 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG", default=0))
-
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
-
-DJANGO_READ_ONLY = False
-# Application definition
-
 
 INSTALLED_APPS = [
     'django.contrib.staticfiles',
